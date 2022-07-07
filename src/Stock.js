2cb0b4f2d8806e870ad459d196fec1c8701a53f3
Krishanu69 <krishanu708@gmail.com>
2022-07-07 22:42:44 +0530

    first commit

---
+++ src/Stock.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class stock extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Stock market</h1>
+      </div>
+    )
+  }
+}
+
+export default stock;
