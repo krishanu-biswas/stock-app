c86f60150e7637473ce0e8427f8b87bd6c11f5b4
Krishanu69 <krishanu708@gmail.com>
2022-07-07 22:51:30 +0530

    first commit

--- src/Stock.js
+++ src/Stock.js
@@ -1,13 +1,73 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Plot from 'react-plotly.js';
 
- class stock extends Component {
+class Stock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stockChartXValues: [],
+      stockChartYValues: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchStock();
+  }
+
+  fetchStock() {
+    const pointerToThis = this;
+    console.log(pointerToThis);
+    const API_KEY = 'CHTYZWT9AR9MVZAP';
+    let StockSymbol = 'TCS';
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${StockSymbol}.BSE&outputsize=full&apikey=${API_KEY}`;
+    let stockChartXValuesFunction = [];
+    let stockChartYValuesFunction = [];
+
+    fetch(API_Call)
+      .then(
+        function(response) {
+          return response.json();
+        }
+      )
+      .then(
+        function(data) {
+          console.log(data);
+
+          for (var key in data['Time Series (Daily)']) {
+            stockChartXValuesFunction.push(key);
+            stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
+          }
+
+          // console.log(stockChartXValuesFunction);
+
+          pointerToThis.setState({
+            stockChartXValues: stockChartXValuesFunction,
+            stockChartYValues: stockChartYValuesFunction
+          });
+        }
+      )
+  }
+
   render() {
     return (
       <div>
-        <h1>Stock market</h1>
+        <h1>Stock Market</h1>
+        <h1>Tata Consultancy Services</h1>
+        <Plot
+          data={[
+            {
+              x: this.state.stockChartXValues,
+              y: this.state.stockChartYValues,
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: {color: 'red'},
+            }
+          ]}
+          layout={{width: 720, height: 440, title: 'A Fancy Plot'}}
+        />
       </div>
     )
   }
 }
 
-export default stock;
+export default Stock;
